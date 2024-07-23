9cf04d276af2624e8b66e5f99c2f7930ff66306a
veronicadeleonh <v.dleon@gmail.com>
2024-07-23 09:54:20 +0200

    components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='items-center max-w-[800px] mx-auto text-xl text-white'>
-        I'm <strog>Verónica</strog>, an <strong>analog photography enthusiast</strong> and <strong>female solo traveler</strong> based in Berlin. Here are some of snaps from my backpacking adventures ✌🏽.
+    <div className='items-center max-w-[800px] mx-auto font-light text-xl text-white'>
+        I'm <span className='font-bold'>Verónica</span>, an <span className='font-bold'>analog photography enthusiast</span> and <span className='font-bold'>female solo traveler</span> based in Berlin. Here are some of snaps from my backpacking adventures ✌🏽.
         </div>
   )
 }
