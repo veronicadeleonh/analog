ac4fc2752572156276bc7710db0e5524800cf8d3
veronicadeleonh <v.dleon@gmail.com>
2024-09-11 15:29:39 +0200

    small about fixes

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -11,7 +11,7 @@ const About = ({ containerSmall, photos }) => {
   return (
     <div className={containerSmall}>
           <Hero /> 
-        <div className='pb-10 font-light text-xl text-zinc-100'>
+        <div className='font-light text-l text-zinc-100'>
           <p className='pb-4'>My passion for solo travel began with my first trip to Cuba in 2018. Since then, I've been fascinated by the thrill of exploring new places 
             and planning my journeys independently. Traveling alone allows me to completely disconnect from my daily routine, giving me the freedom 
             to forge new connections with people<span className='italic'>-and animals—</span>and to explore at my own pace.</p>
@@ -21,12 +21,12 @@ const About = ({ containerSmall, photos }) => {
             understand the world, connecting me to the essence of life in different corners of the globe.</p>
         </div>
 
-            <MapWithPins photos={photos} />
-            
-         <p className='text-zinc-300 text-right text-sm pt-2 pb-4'>World map showing the places I've taken pictures since 2018</p>
+          <div className='pt-10 pb-10'>
+            <MapWithPins photos={photos}/>
+            <p className='text-zinc-300 text-right text-sm pt-2 pb-4'>World map showing the places I've taken pictures since 2018</p>
+         </div>
 
-
-        <CamerasIUse containerSmall={containerSmall} />
+        {/* <CamerasIUse containerSmall={containerSmall} /> */}
         <p className='font-light text-l text-zinc-100'>When I'm not traveling or planning my next adventure, I enjoy designing and building user interfaces.</p>
         <LinkToUXUI />
 
