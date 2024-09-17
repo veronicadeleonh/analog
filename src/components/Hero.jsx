89bae21d8c1dfaf4d2f4c5085a5f81a764368677
veronicadeleonh <v.dleon@gmail.com>
2024-09-17 19:58:32 +0200

    changes in about text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = ({containerSmall}) => {
 
     <div className={containerSmall}>
         <div className='mt-14 md:mt-6 font-light text-2xl text-zinc-100 pb-12'>
-        <p className='pb-4'>Hi, I'm <span className='font-semibold'>Verónica</span>, a Berlin based <span className='font-semibold'>analog photography enthusiast</span> and 
+        <p className='pb-4'>I'm <span className='font-semibold'>Verónica</span>, a Berlin based <span className='font-semibold'>analog photography enthusiast</span> and 
           <span className='font-semibold'> female solo traveler</span>. Here I've collected snaps from my backpacking adventures over the years 🎒📸✌🏽🗺️.</p>
         </div>
       </div>
--- src/views/About.jsx
+++ src/views/About.jsx
@@ -4,17 +4,18 @@ import MapWithPins from "../components/MapWithPins"
 
 import LinkToUXUI from '../components/LinkToUXUI';
 import CamerasIUse from '../components/CamerasIUse';
-import Hero from '../components/Hero'
+
 
 
 const About = ({ containerSmall, photos }) => {
 
   return (
     <div className={containerSmall}>
-          <Hero /> 
+        <h2 className='text-3xl font-semibold pb-4 text-zinc-100'>About me</h2>
         <div className='font-light text-l text-zinc-100'>
-          <p className='pb-4'>My passion for solo travel began with my first trip to Cuba in 2018. Since then, I've been fascinated by the thrill of exploring new places 
-            and planning my journeys independently. Traveling alone allows me to completely disconnect from my daily routine, giving me the freedom 
+          <p className='pb-4'>I'm <span className='font-semibold'>Verónica</span>, an <span className='font-semibold'>analog photography enthusiast</span> and <span className='font-semibold'>female solo traveler</span> based in Berlin. Since my first solo trip to Cuba in 2018, I became fascinated about exploring places on my own.</p>
+
+          <p className='pb-4'>Traveling alone allows me to completely disconnect from my daily routine, giving me the freedom 
             to forge new connections with people<span className='italic'>-and animals—</span>and to explore at my own pace.</p>
 
           <p className='pb-4'>I immerse myself deeply in the cultures I visit, building relationships with locals by sharing 
