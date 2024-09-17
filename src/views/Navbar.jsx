72e43445431c07c3b4ab7bbfbd1a9bd07295373d
veronicadeleonh <v.dleon@gmail.com>
2024-09-17 20:27:13 +0200

    fixing margins

--- src/views/Navbar.jsx
+++ src/views/Navbar.jsx
@@ -9,9 +9,9 @@ const Navbar = ({ containerBig }) => {
   return (
 
    <div className={containerBig}>
-    <div className='md:flex justify-between items-center h-24 text-zinc-50 text-xl font-light'>
+    <div className='md:flex justify-between items-center text-zinc-50 text-xl font-light mt-4'>
         <Link to="/" >
-        <h1 className='w-full font-semibold pb-6 md:pb-0'>Verónica De León Hernández
+        <h1 className='w-full font-semibold pb-4 md:pb-0'>Verónica De León Hernández
             <span className='text-zinc-500 font-light'> analog</span>
         </h1>
         </Link>
