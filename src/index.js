cd9900d0570379a421336b2c2d97cda20921fc60
veronicadeleonh <v.dleon@gmail.com>
2024-09-17 20:16:12 +0200

    vercel analytics / speed insights

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
