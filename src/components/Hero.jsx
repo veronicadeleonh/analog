805753ff4e1aff246d380f6fab1e075c01dbbf28
veronicadeleonh <v.dleon@gmail.com>
2024-09-17 20:35:16 +0200

    mb nav

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = ({containerSmall}) => {
   return (
 
     <div className={containerSmall}>
-        <div className='mt-12 font-light text-2xl text-zinc-100 pb-12'>
+        <div className='font-light text-2xl text-zinc-100 pb-12'>
         <p className='pb-4'>I'm <span className='font-semibold'>Verónica</span>, a Berlin based <span className='font-semibold'>analog photography enthusiast</span> and 
           <span className='font-semibold'> female solo traveler</span>. Here I've collected snaps from my backpacking adventures over the years 🎒📸✌🏽🗺️.</p>
         </div>
--- src/views/Navbar.jsx
+++ src/views/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({ containerBig }) => {
   return (
 
    <div className={containerBig}>
-    <div className='md:flex justify-between items-center text-zinc-50 text-xl font-light mt-4'>
+    <div className='md:flex justify-between items-center text-zinc-50 text-xl font-light mt-6 mb-12'>
         <Link to="/" >
         <h1 className='w-full font-semibold pb-4 md:pb-0'>Verónica De León Hernández
             <span className='text-zinc-500 font-light'> analog</span>
