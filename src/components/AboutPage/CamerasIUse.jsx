d691971c2557e81403a5b1e80b9ef56f1eec1c24
veronicadeleonh <v.dleon@gmail.com>
2024-09-24 14:27:53 +0200

    cameras illustrations

--- src/components/AboutPage/CamerasIUse.jsx
+++ src/components/AboutPage/CamerasIUse.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const CamerasIUse = ({ containterSmall }) => {
 
   let cameras = [
-    {name:'Canonet QL17 III', type:'Rangefinder', size:'35mm'},
-    {name:'Mamiya 645', type:'SRL', size:'120mm'},
-    {name:'Olympus Pen', type:'Point-n-shoot', size:'35mm'},
-    {name:'Canon Prima AS-1', type:'Fully automatic', size:'35mm'},
+    {name:'Canonet QL17 III', type:'Rangefinder', size:'35mm', image:'/cameras/camera-canonet.svg'},
+    {name:'Mamiya 645', type:'SRL', size:'120mm', image:'/cameras/camera-mamiya.svg'},
+    {name:'Olympus Pen', type:'Point-n-shoot', size:'35mm', image:'/cameras/camera-olympus.svg'},
+    {name:'Canon Prima AS-1', type:'Fully automatic', size:'35mm', image:'/cameras/camera-as-1.svg'},
   ]
 
   return (
@@ -14,11 +14,14 @@ const CamerasIUse = ({ containterSmall }) => {
         <h2 className='text-2xl font-semibold pb-2'>Cameras I use</h2>
         <p className='pb-6'>I mostly travel with a small format camera, being the Canonet my favourite one. Depending on the destination and duration of the trip, I also like to bring a second or third camera with me: The waterproof and the halfframe cameras are very fun to have.</p>
 
-        <div className='grid grid-cols-2 gap-2 pb-12'>
+        <div className='sm:grid sm:grid-cols-2 gap-2 pb-12'>
           {cameras.map((camera) => (
-            <div className='py-3 px-4 rounded-sm transition-all duration-150 hover:bg-zinc-900'>
-            <p className='font-bold'>{camera.name}</p>
-            <p className='text-zinc-500'>{camera.type}, {camera.size}</p>
+            <div className='flex gap-3 items-center py-3 px-4 rounded-sm transition-all duration-150 hover:bg-zinc-900'>
+              <img src={camera.image} />
+              <div>
+                <p className='font-bold'>{camera.name}</p>
+                <p className='text-zinc-500'>{camera.type}, {camera.size}</p>
+              </div>
             </div>
         ))}
         </div>
