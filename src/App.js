b130f93115d45e730af62cb96eb757b6b74053da
veronicadeleonh <v.dleon@gmail.com>
2024-11-20 17:49:17 +0100

    loader

--- src/App.js
+++ src/App.js
@@ -14,20 +14,30 @@ const handleContainerBig = "max-w-[1240px] mx-auto sm:p-2 p-3"
 function App() {
 
   const [photos, setPhotos] = useState([])
+  const [loading, setLoading] = useState(true)
   const { getPhotos } = useContentful()
 
   useEffect(() => {
-    getPhotos().then((response) => setPhotos(response))
-  }, [])
+    getPhotos().then((response) => {
+      setPhotos(response);
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <>
       <Navbar containerBig={handleContainerBig} />
+      {loading ? (
+        <div className="fixed inset-0 flex justify-center items-center bg-black/30">
+          <div className="w-12 h-12 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+        </div>
+      ) : (
         <Routes>
           <Route exact path='/' element={<Home containerSmall={handleContainerSmall} containerBig={handleContainerBig} photos={photos} />} />
           <Route exact path='/about' element={<About containerSmall={handleContainerSmall} photos={photos} />} />
           <Route exact path='/contact' element={<GetInTouch containerSmall={handleContainerSmall} />} />
         </Routes>
+      )}
       <Footer />
     </>
   );
